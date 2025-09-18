31547197eeae984989d089fbd3b5892cb5ceda08
SmoothDude6969 <caporin.ale@gmail.com>
2025-09-18 17:18:07 +0200

    Update background.js

--- background.js
+++ background.js
@@ -1,42 +1,17 @@
 const URL_MAP = {
-  "||miniblox.*textures/spritesheet.png": "https://raw.githubusercontent.com/SmoothDude6969/Tpack-for-videos/refs/heads/main/fake_bedrock.png",
-  "||miniblox.*assets/default-DKNlYibk.png": "https://raw.githubusercontent.com/SmoothDude6969/Minibloxtexturespt2/refs/heads/main/DEBUG/custom_background.png",
-  "||miniblox.*assets/miniblox-Dj36hMhG": "https://raw.githubusercontent.com/SmoothDude6969/Minibloxtexturespt2/refs/heads/main/DEBUG/Custom%20writing.png",
-  "||miniblox.*assets/Miniblox-Regular-Lz-C8nWS.otf": "https://raw.githubusercontent.com/SmoothDude6969/MinibloxTexturespt2/main/assets/Miniblox-Regular-Lz-C8nWS.otf",
-  "||miniblox.*textures/entity/chicken/chicken.png": "https://raw.githubusercontent.com/SmoothDude6969/Minibloxtexturespt2/refs/heads/main/entity/chicken.png",
-  "||miniblox.*textures/entity/cow/cow.png": "https://raw.githubusercontent.com/SmoothDude6969/Minibloxtexturespt2/refs/heads/main/entity/cow.png",
-  "||miniblox.*textures/entity/creeper/creeper.png": "https://raw.githubusercontent.com/SmoothDude6969/MinibloxTexturespt2/main/entity/creeper.png",
-  "||miniblox.*textures/entity/pig/pig.png": "https://raw.githubusercontent.com/SmoothDude6969/MinibloxTexturespt2/main/entity/pig.png", 
-  "||miniblox.*textures/entity/sheep/sheep_fur.png": "https://raw.githubusercontent.com/SmoothDude6969/MinibloxTexturespt2/main/entity/sheep_fur.png",
-  "||miniblox.*textures/entity/skeleton/skeleton.png": "https://raw.githubusercontent.com/SmoothDude6969/MinibloxTexturespt2/main/entity/skeleton.png",
-  "||miniblox.*textures/entity/slime/slime.png": "https://raw.githubusercontent.com/SmoothDude6969/MinibloxTexturespt2/main/entity/slime.png",
-  "||miniblox.*textures/entity/spider/spider.png": "https://raw.githubusercontent.com/SmoothDude6969/MinibloxTexturespt2/main/entity/spider.png",
-  "||miniblox.*textures/entity/zombie/zombie.png": "https://raw.githubusercontent.com/SmoothDude6969/Tpack-for-videos/refs/heads/main/sushi.png",
-  "||miniblox.*textures/entity/ghost/ghost.png": "https://raw.githubusercontent.com/SmoothDude6969/MinibloxTexturespt2/main/entity/ghost.png",
-  "||miniblox.*textures/entity/zombie_cowman/zombie_cowman.png": "https://raw.githubusercontent.com/SmoothDude6969/MinibloxTexturespt2/main/entity/zombie_cowman.png",
-  "||miniblox.*textures/entity/snowman/snowman.png": "https://raw.githubusercontent.com/SmoothDude6969/MinibloxTexturespt2/main/entity/snowman.png",
-  "||miniblox.*textures/entity/arrow.png": "https://raw.githubusercontent.com/SmoothDude6969/MinibloxTexturespt2/main/entity/arrow.png",
-  "||miniblox.*textures/entity/boat/acacia.png": "https://raw.githubusercontent.com/SmoothDude6969/MinibloxTexturespt2/main/entity/acacia.png",
-  "||miniblox.*textures/entity/boat/birch.png": "https://raw.githubusercontent.com/SmoothDude6969/MinibloxTexturespt2/main/entity/birch.png",
-  "||miniblox.*textures/entity/boat/dark_oak.png": "https://raw.githubusercontent.com/SmoothDude6969/MinibloxTexturespt2/main/entity/dark_oak.png",
-  "||miniblox.*textures/entity/boat/jungle.png": "https://raw.githubusercontent.com/SmoothDude6969/MinibloxTexturespt2/main/entity/jungle.png",
-  "||miniblox.*textures/entity/boat/oak.png": "https://raw.githubusercontent.com/SmoothDude6969/MinibloxTexturespt2/main/entity/oak.png",
-  "||miniblox.*textures/entity/boat/spruce.png": "https://raw.githubusercontent.com/SmoothDude6969/MinibloxTexturespt2/main/entity/spruce.png",
-  "||miniblox.*textures/models/armor/chainmail_layer_1.png": "https://raw.githubusercontent.com/SmoothDude6969/MinibloxTexturespt2/main/armor/chainmail_layer_1.png",
-  "||miniblox.*textures/models/armor/chainmail_layer_2.png": "https://raw.githubusercontent.com/SmoothDude6969/MinibloxTexturespt2/main/armor/chainmail_layer_2.png",
-  "||miniblox.*textures/models/armor/diamond_layer_1.png": "https://raw.githubusercontent.com/SmoothDude6969/MinibloxTexturespt2/main/armor/diamond_layer_1.png",
-  "||miniblox.*textures/models/armor/diamond_layer_2.png": "https://raw.githubusercontent.com/SmoothDude6969/MinibloxTexturespt2/main/armor/diamond_layer_2.png",
-  "||miniblox.*textures/models/armor/emerald_layer_1.png": "https://raw.githubusercontent.com/SmoothDude6969/MinibloxTexturespt2/main/armor/emerald_layer_1.png",
-  "||miniblox.*textures/models/armor/emerald_layer_2.png": "https://raw.githubusercontent.com/SmoothDude6969/MinibloxTexturespt2/main/armor/emerald_layer_2.png",
-  "||miniblox.*textures/models/armor/infernium_layer_1.png": "https://raw.githubusercontent.com/SmoothDude6969/MinibloxTexturespt2/main/armor/infernium_layer_1.png",
-  "||miniblox.*textures/models/armor/infernium_layer_2.png": "https://raw.githubusercontent.com/SmoothDude6969/MinibloxTexturespt2/main/armor/infernium_layer_2.png",
-  "||miniblox.*textures/models/armor/gold_layer_1.png": "https://raw.githubusercontent.com/SmoothDude6969/MinibloxTexturespt2/main/armor/gold_layer_1.png",
-  "||miniblox.*textures/models/armor/gold_layer_2.png": "https://raw.githubusercontent.com/SmoothDude6969/MinibloxTexturespt2/main/armor/gold_layer_2.png",
-  "||miniblox.*textures/models/armor/iron_layer_1.png": "https://raw.githubusercontent.com/SmoothDude6969/MinibloxTexturespt2/main/armor/iron_layer_1.png",
-  "||miniblox.*textures/models/armor/iron_layer_2.png": "https://raw.githubusercontent.com/SmoothDude6969/MinibloxTexturespt2/main/armor/iron_layer_2.png",
-  "||miniblox.*textures/models/armor/leather_layer_1.png": "https://raw.githubusercontent.com/SmoothDude6969/MinibloxTexturespt2/main/armor/leather_layer_1.png",
-  "||miniblox.*textures/models/armor/leather_layer_2.png": "https://raw.githubusercontent.com/SmoothDude6969/MinibloxTexturespt2/main/armor/leather_layer_2.png",
-  "||miniblox.*textures/misc/enchanted_item_glint.png": "https://raw.githubusercontent.com/SmoothDude6969/Minibloxtexturespt2/refs/heads/main/DEBUG/enchanted_item_glint.png"
+  "||miniblox.*audio/music/blank.webm": "https://github.com/SmoothDude6969/LPMM/blob/main/Emptiness%20Machine-%20Linkin%20Park.mp3",
+  "||miniblox.*audio/music/earth.webm": "https://github.com/SmoothDude6969/LPMM/blob/main/Heavy%20Is%20the%20Crown%20(Official%20Audio)%20-%20Linkin%20Park.mp3",
+  "||miniblox.*audio/music/eclipse.webm": "https://github.com/SmoothDude6969/LPMM/blob/main/In%20The%20End%20%5BOfficial%20HD%20Music%20Video%5D%20-%20Linkin%20Park%20(1).mp3",
+  "||miniblox.*audio/music/firefly.webm": "https://github.com/SmoothDude6969/LPMM/blob/main/Numb%20(Official%20Music%20Video)%20%5B4K%20UPGRADE%5D%20%20Linkin%20Park.mp3",
+  "||miniblox.*audio/music/hellcat.webm": "https://github.com/SmoothDude6969/LPMM/blob/main/Up%20From%20The%20Bottom%20(Official%20Music%20Video)%20-%20Linkin%20Park.mp3",
+  "||miniblox.*audio/music/invincible.webm": "https://github.com/SmoothDude6969/LPMM/blob/main/among-us-role-reveal-sound.mp3",
+  "||miniblox.*audio/music/lets_go.webm": "https://github.com/SmoothDude6969/LPMM/blob/main/among-us-role-reveal-sound.mp3",
+  "||miniblox.*audio/music/linked.webm": "https://github.com/SmoothDude6969/LPMM/blob/main/among-us-role-reveal-sound.mp3",
+  "||miniblox.*audio/music/my_heart.webm": "https://github.com/SmoothDude6969/LPMM/blob/main/among-us-role-reveal-sound.mp3",
+  "||miniblox.*audio/music/nekozilla.webm": "https://github.com/SmoothDude6969/LPMM/blob/main/among-us-role-reveal-sound.mp3",
+  "||miniblox.*audio/music/popsicle.webm": "https://github.com/SmoothDude6969/LPMM/blob/main/among-us-role-reveal-sound.mp3",
+  "||miniblox.*audio/music/seven.webm": "https://github.com/SmoothDude6969/LPMM/blob/main/among-us-role-reveal-sound.mp3",
+  "||miniblox.*audio/music/skyhigh.webm": "https://github.com/SmoothDude6969/LPMM/blob/main/among-us-role-reveal-sound.mp3",
 };
 
 let rules = [];
@@ -69,6 +44,7 @@ chrome.declarativeNetRequest.updateDynamicRules(
     }
   }
 );
+
 
 
 
